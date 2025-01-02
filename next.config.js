2ca3fbfc629be09d305fc152d8f30d3fc1eda730
rizkyharahap <rizkyhh1999@gmail.com>
2025-01-02 21:04:25 +0700

    initial setup

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const bundleAnalyzer = require("@next/bundle-analyzer");
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["selector", '[data-mode="dark"]'],
+
+  content: [
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  corePlugins: {
+    // Remove Tailwind CSS's preflight style so it can use the antd's preflight instead (reset.css).
+    preflight: false,
+  },
+  important: "#app",
+  plugins: [],
+};
+export default config;
